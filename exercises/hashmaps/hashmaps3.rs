1cefb09a9a175a8968614b6000b7ea5f970fe104
Isaac Kilbourne <kilbouri@uwindsor.ca>
2023-05-10 17:50:41 -0400

    Solve up to quiz1.rs

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,11 +14,10 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store team name and its goal details.
+#[derive(Debug)]
 struct Team {
     name: String,
     goals_scored: u8,
@@ -35,12 +34,30 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
-        // TODO: Populate the scores table with details extracted from the
-        // current line. Keep in mind that goals scored by team_1
-        // will be the number of goals conceded from team_2, and similarly
-        // goals scored by team_2 will be the number of goals conceded by
-        // team_1.
+
+        // helper to update a team (or create if not exists)
+        let mut create_or_update_team =
+            |name: String, scored: u8, ceded: u8| match scores.get_mut(&name) {
+                None => {
+                    scores.insert(
+                        name.clone(),
+                        Team {
+                            name,
+                            goals_scored: scored,
+                            goals_conceded: ceded,
+                        },
+                    );
+                }
+                Some(team) => {
+                    team.goals_scored += scored;
+                    team.goals_conceded += ceded;
+                }
+            };
+
+        create_or_update_team(team_1_name, team_1_score, team_2_score);
+        create_or_update_team(team_2_name, team_2_score, team_1_score);
     }
+
     scores
 }
 
